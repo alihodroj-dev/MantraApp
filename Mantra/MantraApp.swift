cca9a2fb309faa2650dc325ea47b23a2c7f0afc9
Ali Hodroj <aloshito2004@gmail.com>
2024-05-25 17:45:46 +0300

    Initial Commit

---
+++ Mantra/MantraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MantraApp.swift
+//  Mantra
+//
+//  Created by Ali Hodroj on 25/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MantraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
